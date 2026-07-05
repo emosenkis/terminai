77e190ef4638a3ab2e9515d03fa450240077b6e0
Eitan Mosenkis <eitan@mosenkis.net>
2026-07-05 23:34:27 +0300

    Pin Homebrew formula to v0.1.0

--- Formula/terminai.rb
+++ Formula/terminai.rb
@@ -5,16 +5,18 @@ class Terminai < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/emosenkis/terminai/releases/download/v0.1.0/terminai-0.1.0-darwin-aarch64.tar.gz"
+      sha256 "ea07f74779bd2c94f7dadb0b54dcbe13e4ba7a91d22a482c2d44099afdcb23dd"
     else
       url "https://github.com/emosenkis/terminai/releases/download/v0.1.0/terminai-0.1.0-darwin-x86_64.tar.gz"
+      sha256 "a240b7f950cb07b66a062ce590ba8d0ccbbc704ad14edf974ffe4d420942ead4"
     end
   elsif OS.linux?
     odie "Terminai currently ships Linux binaries for x86_64 only" unless Hardware::CPU.intel?
 
     url "https://github.com/emosenkis/terminai/releases/download/v0.1.0/terminai-0.1.0-linux-x86_64-musl.tar.gz"
+    sha256 "13208c59faa0c4dd911cc0bb21def79fd47c84200ab1cf426c54be4ad03deb9c"
   end
 
-  sha256 :no_check
   license "MIT"
 
   def install
